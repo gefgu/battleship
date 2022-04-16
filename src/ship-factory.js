1a516c9447afe369b637b09eae5c63bf2b2f9769
gefgu <gefgu@hotmail.com>
2022-04-16 20:10:49 -0300

    Add ship factory function implementation

---
+++ src/ship-factory.js
@@ -0,0 +1,13 @@
+const createShip = (length) => {
+  let hitLocations = Array.from({length: length}, () => false)
+  return {
+    hit: (position) => {
+      hitLocations[position] = true;
+    },
+    isSunk: () => {
+      return hitLocations.reduce((prev, curr) => prev && curr, true);
+    }
+  }
+};
+
+export { createShip };
--- src/ship-factory.test.js
+++ src/ship-factory.test.js
@@ -1,19 +1,21 @@
+import { createShip } from "./ship-factory";
+
 test("Sunk ship of length 1 after 1 hit", () => {
-  const myShip = Ship(1);
+  const myShip = createShip(1);
   myShip.hit(0);
   expect(myShip.isSunk()).toBe(true);
-})
+});
 
 test("Don't sunk ship of length 3 after 1 hit", () => {
-  const myShip = Ship(3);
+  const myShip = createShip(3);
   myShip.hit(1);
   expect(myShip.isSunk()).toBe(false);
-})
+});
 
 test("Sunk ship of length 3 after 3 hit", () => {
-  const myShip = Ship(1);
+  const myShip = createShip(1);
   myShip.hit(1);
   myShip.hit(2);
   myShip.hit(0);
   expect(myShip.isSunk()).toBe(true);
-})
+});
