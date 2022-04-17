f92dc047810167f7e08d1c69d55c89a0f031a7d8
gefgu <gefgu@hotmail.com>
2022-04-17 09:44:42 -0300

    Add gameboard tests

---
+++ src/gameboard-factory.test.js
@@ -0,0 +1,40 @@
+test("All sunk after attack in 1 ship of length 1", () => {
+  const gameboard = createGameboard();
+  gameboard.placeShip(0, 0, 1);
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.allSunk()).toBe(true);
+});
+
+test("Not all sunk after missed attack in 1 ship of length 1", () => {
+  const gameboard = createGameboard();
+  gameboard.placeShip(0, 0, 1);
+  gameboard.receiveAttack(1, 0);
+  expect(gameboard.allSunk()).toBe(false);
+});
+
+test("All sunk after attack in 2 ship of length 3", () => {
+  const gameboard = createGameboard();
+  gameboard.placeShip(0, 0, 3);
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(1, 0);
+  gameboard.receiveAttack(2, 0);
+
+  gameboard.placeShip(0, 1, 3);
+  gameboard.receiveAttack(0, 1);
+  gameboard.receiveAttack(1, 2);
+  gameboard.receiveAttack(2, 3);
+  expect(gameboard.allSunk()).toBe(true);
+});
+
+test("Report all shots", () => {
+  const gameboard = createGameboard();
+  gameboard.placeShip(0, 0, 3);
+  gameboard.receiveAttack(0, 1);
+  gameboard.receiveAttack(1, 0);
+  gameboard.receiveAttack(2, 2);
+  expect(gameboard.reportShots()).toBe([
+    [0, 1],
+    [1, 0],
+    [2, 2],
+  ]);
+});
