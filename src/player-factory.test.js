6ab8567a873fa55a8879d369c1363b2ef74f6b29
gefgu <gefgu@hotmail.com>
2022-04-19 19:27:16 -0300

    Change tests of player for less shared state

--- src/player-factory.test.js
+++ src/player-factory.test.js
@@ -4,17 +4,15 @@ import { createGameboard } from "./gameboard-factory";
 test("Make Sucessful Random attack", () => {
   const botPlayer = createPlayer();
   const enemyGameboard = createGameboard();
-  botPlayer.setEnemyGameboard(enemyGameboard);
-  botPlayer.attack();
+  botPlayer.attack(enemyGameboard);
   expect(enemyGameboard.reportShots().length).toBe(1);
 });
 
 test("Make 10 attacks without repetition", () => {
   const botPlayer = createPlayer();
   const enemyGameboard = createGameboard();
-  botPlayer.setEnemyGameboard(enemyGameboard);
   for (let i = 0; i < 10; i++) {
-    botPlayer.attack();
+    botPlayer.attack(enemyGameboard);
   }
   expect(enemyGameboard.reportShots().length).toBe(10);
 });
