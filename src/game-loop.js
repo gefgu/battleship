4817775c84dd665cb8f3f1b1afa5bb6afe19d38a
gefgu <gefgu@hotmail.com>
2022-04-19 19:25:10 -0300

    Add gameboard setup in game loop

---
+++ src/game-loop.js
@@ -0,0 +1,24 @@
+import { createPlayer } from "./player-factory";
+import { createGameboard } from "./gameboard-factory";
+
+const gameController = () => {
+  const run = () => {
+    const player1 = createPlayer();
+    const player2 = createPlayer();
+    const player1Gameboard = createGameboard();
+    player1Gameboard.placeShip(0, 0, 5); // Carrier
+    player1Gameboard.placeShip(3, 4, 4); // Battleship
+    player1Gameboard.placeShip(7, 2, 3); // Destroyer
+    player1Gameboard.placeShip(2, 8, 3); // Submarine
+    player1Gameboard.placeShip(5, 5, 2); // Patrol Boat
+    const player2Gameboard = createGameboard();
+    player2Gameboard.placeShip(5, 3, 5); // Carrier
+    player2Gameboard.placeShip(6, 1, 4); // Battleship
+    player2Gameboard.placeShip(0, 4, 3); // Destroyer
+    player2Gameboard.placeShip(4, 8, 3); // Submarine
+    player2Gameboard.placeShip(7, 5, 2); // Patrol Boat
+    player1.setEnemyGameboard(player2Gameboard);
+    player2.setEnemyGameboard(player1Gameboard);
+  };
+  return { run };
+};
